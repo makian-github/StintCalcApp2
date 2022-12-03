06eab300ae44587be8918ce2fa48523fb2109245
makian <makian.gpc>
2022-12-03 22:13:09 +0900

    レース情報で設定できる項目を管理するRaceData.javaを追加

--- app/src/main/java/com/example/stintcalcapp2/model/RaceData.java
+++ app/src/main/java/com/example/stintcalcapp2/model/RaceData.java
@@ -1,4 +1,37 @@
 package com.example.stintcalcapp2.model;
 
 public class RaceData {
+
+    private int raceTime = 0;
+    private int stint = 0;
+    private String startTime = "00:00";
+
+    public RaceData() {
+    }
+
+    public int getRaceTime() {
+        return raceTime;
+    }
+
+    public void setRaceTime(int raceTime) {
+        this.raceTime = raceTime;
+    }
+
+    public int getStint() {
+        return stint;
+    }
+
+    public void setStint(int stint) {
+        this.stint = stint;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
 }
+
+
